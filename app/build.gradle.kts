600c8156b7a9b6785a9ab93ebc401737c155d8f3
Kolbe <Kolbel@gmail.com>
2026-07-06 16:40:21 -0600

    Bump to v2.2.1 (versionCode 4) for Play Store submission

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "com.quc.app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 3
-        versionName = "2.2.0"
+        versionCode = 4
+        versionName = "2.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
